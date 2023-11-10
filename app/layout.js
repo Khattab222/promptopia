c1e24694af504f39801d26172c7918b03a9a5a60
khaled khattab <91126212+Khattab222@users.noreply.github.com>
2023-11-10 18:04:02 +0200

    starting

---
+++ app/layout.js
@@ -0,0 +1,32 @@
+import { Inter } from 'next/font/google'
+import '@styles/globals.css'
+
+import Provider from '@components/Provider'
+import Nav from '@components/Nav'
+
+
+
+
+export const metadata = {
+  title: 'promptopia',
+  description: 'Discover and share AI Prompts',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body >
+        <div className='main'>
+
+        <div className='gradients'/>
+        </div>
+        <main className='app'>
+          <Nav/>
+        {children}
+        </main>
+
+       
+      </body>
+    </html>
+  )
+}
---
+++ components/Nav.jsx
@@ -0,0 +1,14 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import {signIn,signOut,useSession,getProviders} from 'next-auth/react'
+
+
+const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav
