3d7dd2b027840a8aed88013ce2b82993ae981edd
Khattab222 <khattabk205@gmail.com>
2023-11-16 14:57:07 +0200

    starting working in create prompt

---
+++ app/create-prompt/page.js
@@ -0,0 +1,18 @@
+'use client'
+import Form from '@components/Form'
+import React, { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
+
+const createPrompt = () => {
+
+   
+
+    
+  return (
+    <div>create-prompt</div>
+  )
+}
+
+export default createPrompt
